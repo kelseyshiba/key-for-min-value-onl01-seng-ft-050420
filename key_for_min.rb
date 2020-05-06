60726ead04064516f9378812d14e8050626cf169
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-06 17:55:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,6 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, value|
     if smallest = nil || value < smallest
       value = smallest
-      smallest = name
     end
   end
   smallest
