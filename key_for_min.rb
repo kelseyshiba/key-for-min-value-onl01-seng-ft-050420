01212b85caf5f1fe482dfbab890a525c1e47bf2f
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-06 18:29:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   smallest = nil
   name_hash.each do |name, value|
+    binding.pry
     if smallest == nil || value < smallest
       value = smallest
       smallest = name
